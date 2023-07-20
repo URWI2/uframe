a1c3432aa90e5443282a9e3c3090f9d51e2910c3
ChristianAmes <christian.amesoeder@informatik.uni-regensburg.de>
2023-07-20 16:37:06 +0200

    updated project to 0.0.5

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup()
+    install_requires = [] 
+    with open("requirements.txt") as f:
+        install_requires = f.read().splitlines()
+    setup(install_requires=install_requires)
