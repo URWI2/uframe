d0c538f7dd9b5c268db28d6b9e0778091cf0af7e
ChristianAmes <christian.amesoeder@informatik.uni-regensburg.de>
2024-02-05 17:33:36 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../src/uframe'))
 
-
+print(os.path.abspath('../../src/uframe'))
 
 project = 'uframe'
 copyright = '2024, Christian Amesoeder, Michael Hagn'
