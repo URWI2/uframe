e46468d69ee3ae8710c11a2baaf346b4598e8a57
ChristianAmes <christian.amesoeder@informatik.uni-regensburg.de>
2024-02-05 17:18:28 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,11 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src/uframe'))
+
+
 
 project = 'uframe'
 copyright = '2024, Christian Amesoeder, Michael Hagn'
@@ -37,11 +38,4 @@ autodoc_typehints = "description"
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-
-import os
-import sys
-
-
-sys.path.insert(0, os.path.abspath('/src/uframe/'))
-sys.path.insert(0, os.path.abspath('../src/uframe/'))
 
